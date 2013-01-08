3459131a8464b2f54aea1f7f83c657da9c51a6a3
Gray Ghost Visuals <grayghost@grayghostvisuals.com>
2013-01-07 22:00:53 -0500

    Unique Compass Config File and removed from Grunt options

--- yeoman/Gruntfile.js
+++ yeoman/Gruntfile.js
@@ -32,12 +32,15 @@ module.exports = function( grunt ) {
             dist: {
                 // http://compass-style.org/help/tutorials/configuration-reference/#configuration-properties
                 options: {
+                    config: 'config.rb'
+                    // uncomment the lines below if you don't
+                    // wanna use the config.rb file
+                    //
                     //css_dir: 'temp/styles',
-                    css_dir: 'app/styles',
-                    sass_dir: 'app/styles',
-                    images_dir: 'app/images',
-                    javascripts_dir: 'temp/scripts',
-                    force: true
+                    //sass_dir: 'app/styles',
+                    //images_dir: 'app/images',
+                    //javascripts_dir: 'temp/scripts',
+                    //force: true
                 }
             }
         },
